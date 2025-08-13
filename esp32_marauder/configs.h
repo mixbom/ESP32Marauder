8d999a003c080ff8fad6fe3f0263cc3cbacfd46f
mixbom <167911825+mixbom@users.noreply.github.com>
2025-08-13 02:16:38 -0400

    Update configs.h

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -382,7 +382,7 @@
     //#define HAS_BATTERY
     //#define HAS_BT
     //#define HAS_BUTTONS
-    //#define HAS_NEOPIXEL_LED
+    #define HAS_NEOPIXEL_LED
     //#define HAS_PWR_MGMT
     //#define HAS_SCREEN
     #define HAS_GPS
